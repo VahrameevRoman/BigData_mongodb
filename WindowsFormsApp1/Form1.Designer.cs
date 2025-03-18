9bbe0f18196fe6668267793d252c1dc5a6bc8ae2
Roman Vahrameev <romance.vah@mail.ru>
2025-03-18 12:16:18 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolbtnadd = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolbtnedit = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolbtndel = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolbtnsave = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(447, 289);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.AutoSize = false;
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolbtnadd,
+            this.toolStripSeparator1,
+            this.toolbtnedit,
+            this.toolStripSeparator2,
+            this.toolbtndel,
+            this.toolStripSeparator3,
+            this.toolbtnsave,
+            this.toolStripSeparator4});
+            this.toolStrip1.Location = new System.Drawing.Point(465, 249);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(251, 52);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolbtnadd
+            // 
+            this.toolbtnadd.AutoSize = false;
+            this.toolbtnadd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolbtnadd.Image = ((System.Drawing.Image)(resources.GetObject("toolbtnadd.Image")));
+            this.toolbtnadd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolbtnadd.Name = "toolbtnadd";
+            this.toolbtnadd.Size = new System.Drawing.Size(50, 50);
+            this.toolbtnadd.Text = "Добавить";
+            this.toolbtnadd.Click += new System.EventHandler(this.toolbtnadd_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 52);
+            // 
+            // toolbtnedit
+            // 
+            this.toolbtnedit.AutoSize = false;
+            this.toolbtnedit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolbtnedit.Image = ((System.Drawing.Image)(resources.GetObject("toolbtnedit.Image")));
+            this.toolbtnedit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolbtnedit.Name = "toolbtnedit";
+            this.toolbtnedit.Size = new System.Drawing.Size(50, 50);
+            this.toolbtnedit.Text = "Редактировать";
+            this.toolbtnedit.Click += new System.EventHandler(this.toolbtnedit_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 52);
+            // 
+            // toolbtndel
+            // 
+            this.toolbtndel.AutoSize = false;
+            this.toolbtndel.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolbtndel.Image = ((System.Drawing.Image)(resources.GetObject("toolbtndel.Image")));
+            this.toolbtndel.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolbtndel.Name = "toolbtndel";
+            this.toolbtndel.Size = new System.Drawing.Size(50, 50);
+            this.toolbtndel.Text = "Удалить";
+            this.toolbtndel.Click += new System.EventHandler(this.toolbtndel_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 52);
+            // 
+            // toolbtnsave
+            // 
+            this.toolbtnsave.AutoSize = false;
+            this.toolbtnsave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolbtnsave.Image = ((System.Drawing.Image)(resources.GetObject("toolbtnsave.Image")));
+            this.toolbtnsave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolbtnsave.Name = "toolbtnsave";
+            this.toolbtnsave.Size = new System.Drawing.Size(50, 50);
+            this.toolbtnsave.Text = "Сохранить";
+            this.toolbtnsave.Click += new System.EventHandler(this.toolbtnsave_Click);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 52);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 307);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(704, 138);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ввод сведений о книге";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(122, 98);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(280, 26);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(99, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(303, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(99, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(303, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Год издания:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Название:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Автор:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(720, 456);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Library";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolbtnadd;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolbtnedit;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton toolbtndel;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripButton toolbtnsave;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static private MongoClient client;
+        private IMongoDatabase database;
+        private IMongoCollection<BsonDocument> users;
+        List<BsonDocument> res;
+        DataTable dt;
+        string id;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Getbibl()
+        {
+            users = database.GetCollection<BsonDocument>("books"); // получаем коллекцию books из БД
+            res = users.Find(new BsonDocument()).Sort("{author:1}").ToList(); // получаем список документов из коллекции
+            dt = new DataTable(); // Создаем источник данных для dataGridView
+            dt.Columns.Add("ID");
+            dt.Columns.Add("Автор");
+            dt.Columns.Add("Название");
+            dt.Columns.Add("Год издания");
+            foreach (BsonDocument doc in res)  // Добавляем в источник данные из коллекции
+            {
+                dt.Rows.Add(doc["_id"], doc["author"], doc["bookname"], doc["godizd"]);
+            }
+            dataGridView1.DataSource = dt; // отображаем данные в dataGridView
+            dataGridView1.Columns["ID"].Visible = false;
+        }
+
+        // Загрузка формы
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new MongoClient("mongodb://localhost:27017");
+            database = client.GetDatabase("library");
+            Getbibl();
+        }
+
+        private void toolbtnadd_Click(object sender, EventArgs e)
+        {
+            // Создаем новый документ
+            BsonDocument d = new BsonDocument {
+                {"author", textBox1.Text },
+                {"bookname", textBox2.Text},
+                {"godizd", textBox3.Text }
+            };
+            users.InsertOneAsync(d); // и добавляем его в коллекцию
+            Getbibl();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void toolbtnedit_Click(object sender, EventArgs e)
+        {
+            int ind = dataGridView1.CurrentRow.Index;
+            id = Convert.ToString(dt.Rows[ind][0]);
+            textBox1.Text = Convert.ToString(dt.Rows[ind][1]);
+            textBox2.Text = Convert.ToString(dt.Rows[ind][2]);
+            textBox3.Text = Convert.ToString(dt.Rows[ind][3]);
+        }
+
+        private void toolbtnsave_Click(object sender, EventArgs e)
+        {
+            BsonDocument filter = new BsonDocument(); // создаем фильтр
+            filter.Add("_id", ObjectId.Parse(id)); // указываем, что будет редактироваться запись с нужным значением ключа
+            // создаем новый документ для замены
+            BsonDocument d = new BsonDocument {
+            {"author", textBox1.Text },
+            {"bookname", textBox2.Text},
+            {"godizd", textBox3.Text }
+            };
+            users.ReplaceOneAsync(filter, d); // собственно редактируем
+            Getbibl();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void toolbtndel_Click(object sender, EventArgs e)
+        {
+            int ind = dataGridView1.CurrentRow.Index;
+            id = Convert.ToString(dt.Rows[ind][0]);
+            string name = "Вы действительно хотите удалить книгу " + Convert.ToString(dt.Rows[ind][2]) + "?";
+            DialogResult r = MessageBox.Show(name, "Подтвердите...", 
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question, 
+                MessageBoxDefaultButton.Button2, 
+                MessageBoxOptions.DefaultDesktopOnly);
+            if (r == DialogResult.No) return;
+            BsonDocument filter = new BsonDocument();
+            filter.Add("_id", ObjectId.Parse(id));
+            users.DeleteOneAsync(filter);
+            Getbibl();
+        }
+    }
+}
